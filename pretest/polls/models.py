40b8dad70b0bfdeb50c05c065c3053662fd3091a
Nakagawa Shinichi <98950032+shinpochi2002@users.noreply.github.com>
2023-02-24 13:43:33 +0900

    [add]model編集

--- pretest/polls/models.py
+++ pretest/polls/models.py
@@ -1,15 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-from django.db import models
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+# Create your models here
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Stadium(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='stadium_images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    totalrating = models.IntegerField(default=0,)
+    foodrating = models.IntegerField(default=0,)
+    accessrating = models.IntegerField(default=0,)
+    visibilityrating = models.IntegerField(default=0,)
+    passionrating = models.IntegerField(default=0,)
+
+
+class Review(models.Model):
+    totalrating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    foodrating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    accessrating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    visibilityrating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    passionrating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE)
+
